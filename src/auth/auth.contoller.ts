3b4aa8b0db1c97bbb2193f537eb3774d437d3d61
Junior Nunes <nunes.junior92@gmail.com>
2023-04-03 07:36:21 -0300

    add jwt create token and fix problems

--- src/auth/auth.contoller.ts
+++ src/auth/auth.contoller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { AuthForgetDTO } from './dto/auth-forget.dto';
 import { AuthLoginDTO } from './dto/auth-login.dto';
@@ -8,10 +7,7 @@ import { AuthResetDTO } from './dto/auth-reset.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly userService: UserService,
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   async login(@Body() { email, password }: AuthLoginDTO) {
@@ -20,7 +16,7 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() body: AuthRegisterDTO) {
-    return this.userService.create(body);
+    return this.authService.register(body);
   }
 
   @Post('forget')
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.contoller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
@@ -13,5 +14,6 @@ import { AuthController } from './auth.contoller';
     PrismaModule,
   ],
   controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
