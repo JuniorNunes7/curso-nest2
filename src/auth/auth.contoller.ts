4a82c90a6069d5c66ef3065a36bf01a850029fb7
Junior Nunes <nunes.junior92@gmail.com>
2023-07-04 13:15:53 -0300

    add photo upload

--- src/auth/auth.contoller.ts
+++ src/auth/auth.contoller.ts
@@ -1,5 +1,19 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { join } from 'path';
 import { User } from 'src/decorators/user.decorator';
+import { FileService } from 'src/file/file.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { AuthService } from './auth.service';
 import { AuthForgetDTO } from './dto/auth-forget.dto';
@@ -9,7 +23,10 @@ import { AuthResetDTO } from './dto/auth-reset.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly fileService: FileService,
+  ) {}
 
   @Post('login')
   async login(@Body() { email, password }: AuthLoginDTO) {
@@ -35,5 +52,42 @@ export class AuthController {
   @Post('me')
   async me(@User() user) {
     return user;
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @UseGuards(AuthGuard)
+  @Post('photo')
+  async uploadPhoto(
+    @User() user,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({
+            fileType: /^image\/(png|jpeg|jpg)$/,
+          }),
+          new MaxFileSizeValidator({
+            maxSize: 1000000,
+          }),
+        ],
+      }),
+    )
+    photo: Express.Multer.File,
+  ): Promise<{ success: boolean }> {
+    const path = join(
+      __dirname,
+      '..',
+      '..',
+      'storage',
+      'photos',
+      `photo-${user.id}.png`,
+    );
+
+    try {
+      await this.fileService.upload(photo, path);
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+
+    return { success: true };
   }
 }
