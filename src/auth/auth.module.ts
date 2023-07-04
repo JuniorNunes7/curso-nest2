e2f4d2d7286e3d557ea6e3fbe52a197b30d5d161
Junior Nunes <nunes.junior92@gmail.com>
2023-07-04 12:19:49 -0300

    add .env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'TT3$+`uS9R7rk9b@FerM[pB)t`}$(yy{t',
+      secret: process.env.JWT_SECRET,
     }),
     forwardRef(() => UserModule),
     PrismaModule,
