8fa3780e59e80126744d8d8ce056ba3f06497b60
Junior Nunes <nunes.junior92@gmail.com>
2023-07-12 07:37:51 -0300

    add migrations

---
+++ typeorm/data.source.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  migrations: [`${__dirname}/migrations/**/*.ts`],
+});
+
+export default dataSource;
